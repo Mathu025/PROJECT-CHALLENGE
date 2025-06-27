9511423b688f4e3677062af28fff79c46928abb3
Mathu025 <mathualex72@gmail.com>
2025-06-27 20:39:22 +0300

    Downgrade tailwind as v4 doesnt support switching light/dark mode, create tailwind.config.js, make necessary changes to index.js and index.html to ensure everything is in working order

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ function renderGames(games = allGames) {
         div.setAttribute('data-platform', game.platform)
 
         div.innerHTML = `
-            <img class="border rounded" src="${game.image}" alt="${game.title} width="350" height="250"><br>
+            <img class="border rounded" src="${game.image}" alt="${game.title}" width="350" height="250"><br>
             <h3 class="text-2xl mb-2"><strong>${game.title}</strong></h3>
             <p><strong>Genre:</strong> ${game.genre}</p>
             <p><strong>Platform:</strong> ${game.platform}</p>
             <p><strong>Rating:</strong> ${game.rating}</p>
-            <button class="button mt-2 px-3 py-1 hover: bg-blue-500 text-white rounded cursor-pointer">View Details</button>
+            <button class="button mt-2 px-3 py-1 bg-blue-500 hover:bg-gray-500 text-white rounded cursor-pointer">View Details</button>
             <div class="content mt-2 hidden">
                 <p><strong>Description:</strong> ${game.description}</p>
             </div>
@@ -57,14 +57,14 @@ function renderGames(games = allGames) {
 }
 
 function handleFilterorSearch() {
-    const SelectedGenre = genreFilter.value.toLowerCase();
-    const SelectedPlatform = platformFilter.value.toLowerCase();
+    const selectedGenre = genreFilter.value.toLowerCase();
+    const selectedPlatform = platformFilter.value.toLowerCase();
     const searchTerm = searchInput.value.toLowerCase();
 
     const filtered = allGames.filter(game => {
         const matchesSearch = game.title && game.title.toLowerCase().includes(searchTerm);
-        const matchesGenre = SelectedGenre === '' || game.genre && game.genre.toLowerCase() === SelectedGenre;
-        const matchesPlatform = SelectedPlatform === '' || game.platform && game.platform.toLowerCase() === SelectedPlatform;
+        const matchesGenre = selectedGenre === '' || game.genre && game.genre.toLowerCase() === selectedGenre;
+        const matchesPlatform = selectedPlatform === '' || game.platform && game.platform.toLowerCase() === selectedPlatform;
         
         return matchesGenre && matchesPlatform && matchesSearch;
     });
@@ -75,4 +75,29 @@ genreFilter.addEventListener('change', handleFilterorSearch);
 platformFilter.addEventListener('change', handleFilterorSearch);
 searchInput.addEventListener('input', handleFilterorSearch);
 
-document.addEventListener('DOMContentLoaded', fetchGames);
+const theme = document.getElementById('theme');
+const htmlElement = document.documentElement;
+
+theme.addEventListener('click', () => {
+    const isDark = htmlElement.classList.contains('dark');
+    htmlElement.classList.toggle('dark');
+
+    theme.textContent = isDark ? "Switch to Dark Mode" : "Switch to Light Mode";
+    localStorage.setItem('theme', isDark ? 'light' : 'dark')
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'light') {
+        htmlElement.classList.remove('dark');
+        theme.textContent = 'Switch to Dark Mode'
+    }
+    else {
+        htmlElement.classList.add('dark');
+        theme.textContent = 'Switch to Light Mode'
+    }
+    fetchGames();
+})
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    darkMode: 'class',
+    content: ['./index.html', './*.js'],
+    theme: {
+        extend: {},
+    },
+    plugins: []
+};
