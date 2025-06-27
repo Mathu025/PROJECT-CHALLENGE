b580ec9c435111b7d55c39ebfd7ba216c02a506e
Mathu025 <mathualex72@gmail.com>
2025-06-27 16:43:13 +0300

    Add fetchgames function to retrieve and render game data from API

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ const searchInput = document.getElementById('searchInput');
 const platformFilter = document.getElementById('platformFilter');
 const genreFilter = document.getElementById('genreFilter');
 const gameList = document.getElementById('gameList')
+
+let allGames = [];
+
+function fetchGames() {
+    fetch(baseUrl)
+        .then(res => res.json())
+        .then(data => {
+            allGames = data;
+            renderGames(data);
+        })
+        .catch(err => console.error('Failed to load games from the server!', err));
+}
