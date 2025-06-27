f925c26826178210c58166e0e5e017eb5bbd53f0
Mathu025 <mathualex72@gmail.com>
2025-06-27 16:29:47 +0300

    Initialize  DOM elements for search input, genre and platform filters and the whole game list

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-const baseUrl = "https://json-server-1-u08x.onrender.com/games"
+const baseUrl = "https://json-server-1-u08x.onrender.com/games";
 
+const searchInput = document.getElementById('searchInput');
+const platformFilter = document.getElementById('platformFilter');
+const genreFilter = document.getElementById('genreFilter');
+const gameList = document.getElementById('gameList')
